bddf068426bcd40a130a67c524d5723f2fea1727
shreyshah117 <shreyshah1712002@gmail.com>
2024-03-16 10:19:10 -0400

    SpellCHeck and WordCompletion

--- src/main/java/com/HotelHawk/Spring/Controller/CrawlerController.java
+++ src/main/java/com/HotelHawk/Spring/Controller/CrawlerController.java
@@ -4,6 +4,7 @@ import com.HotelHawk.Spring.Crawler.Booking_crawler;
 import com.HotelHawk.Spring.Crawler.Hotelsca_crawler;
 import com.HotelHawk.Spring.Parser.Booking_parser;
 import com.HotelHawk.Spring.Parser.Hotelsca_parser;
+import com.HotelHawk.Spring.spellcheck.SpellCheck;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,12 @@ public class CrawlerController {
 /// user give http reques with city name
     @RequestMapping("/booking_crawler")
     public void booking_crawl(String cityname) throws IOException {
-        Booking_crawler.extract_cities(cityname);
+        Booking_crawler.extract_cities(SpellCheck.initialize(cityname));
         Booking_parser.extract_links();
     }
     @RequestMapping("/hotel_crawler")
     public void hotel_crawl(String cityname) throws IOException, InterruptedException {
-        Hotelsca_crawler.cities(cityname);
+        Hotelsca_crawler.cities(SpellCheck.initialize(cityname));
         Hotelsca_parser.extract_links();
     }
     @RequestMapping("/mmt_crawler")
--- src/main/java/com/HotelHawk/Spring/spellcheck/SpellCheck.java
+++ src/main/java/com/HotelHawk/Spring/spellcheck/SpellCheck.java
@@ -1,4 +1,63 @@
 package com.HotelHawk.Spring.spellcheck;
 //done for request HTTP
+import org.checkerframework.checker.units.qual.A;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class SpellCheck {
+    public static ArrayList<String> cities= new ArrayList<String>();
+    public static ArrayList<Integer> dist =new ArrayList<Integer>();
+    public static String initialize(String cityname){
+        cities.add("TORONTO");
+        cities.add("OTTAWA");
+        cities.add("CALGARY");
+        cities.add("VANCOUVER");
+        cities.add("MONTREAL");
+        return edit_distance("voncver".toUpperCase());
+    }
+    static int min(int x, int y, int z)
+    {
+        if (x <= y && x <= z)
+            return x;
+        if (y <= x && y <= z)
+            return y;
+        else
+            return z;
+    }
+    private static int edit_dist(String city, String find, int m,int n){
+        if (m == 0)
+            return n;
+        if (n == 0)
+            return m;
+        if (city.charAt(m - 1) == find.charAt(n - 1))
+            return edit_dist(city, find, m - 1, n - 1);
+        return 1
+                + min(edit_dist(city, find, m, n - 1), // Insert
+                edit_dist(city, find, m - 1, n), // Remove
+                edit_dist(city, find, m - 1, n - 1));//replace
+    }
+    public static String edit_distance(String find){
+        for(String s:cities){
+            int temp=edit_dist(s,find,s.length(),find.length());
+            System.out.println(String.valueOf(temp)+" "+s+" "+find);
+            dist.add(temp);
+        }
+        System.out.println(cities.get(dist.indexOf(Collections.min(dist))));
+
+        return cities.get(dist.indexOf(Collections.min(dist)));
+    }
+    public static void main(String[] args){
+        cities.add("TORONTO");
+        cities.add("OTTAWA");
+        cities.add("CALGARY");
+        cities.add("VANCOUVER");
+        cities.add("MONTREAL");
+        ArrayList<Integer> dist = new ArrayList<Integer>();
+
+    }
 }
+
